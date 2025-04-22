ed40855e49a11a117402deaa059e0020a57b6a25
Quyet Pham <phammanhquyetx9@gmail.com>
2025-04-22 23:01:38 +0700

    update be

---
+++ be/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+app.use(cors({ origin: 'http://localhost:3006' }));
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
