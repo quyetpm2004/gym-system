dc1eac10b904c22428e60455170c41f44ce81926
quynnchan <quynhle772k4@gmail.com>
2025-06-11 11:54:34 +0700

    ref: button

--- fe/src/components/Button/ActionButtons.jsx
+++ fe/src/components/Button/ActionButtons.jsx
@@ -1,17 +1,35 @@
-import { MdEdit } from "react-icons/md";
-import { FaRegTrashAlt } from "react-icons/fa";
+import { MdEdit } from 'react-icons/md';
+import { FaRegTrashAlt } from 'react-icons/fa';
 
 const ActionButtons = ({ onEdit, onDelete }) => {
-  return (
-    <div>
-      <button className="btn btn-sm me-1" style={{background: '#25bfec'}} onClick={onEdit}>
-        <MdEdit size={16} color="#fff" />
-      </button>
-      <button className="btn btn-sm" style={{background: '#25bfec'}} onClick={onDelete}>
-        <FaRegTrashAlt size={16} color="#fff" />
-      </button>
-    </div>
-  );
+    return (
+        <div>
+            <button
+                className="btn btn-sm me-1 pb-2"
+                style={{ background: '#418fba' }}
+                onClick={onEdit}
+                onMouseEnter={(e) => {
+                    e.currentTarget.style.background = '#22c55e';
+                }}
+                onMouseLeave={(e) => {
+                    e.currentTarget.style.background = '#418fba';
+                }}>
+                <MdEdit size={16} color="#fff" />
+            </button>
+            <button
+                className="btn btn-sm pb-2"
+                style={{ background: '#418fba' }}
+                onClick={onDelete}
+                onMouseEnter={(e) => {
+                    e.currentTarget.style.background = '#d9534f';
+                }}
+                onMouseLeave={(e) => {
+                    e.currentTarget.style.background = '#418fba';
+                }}>
+                <FaRegTrashAlt size={16} color="#fff" />
+            </button>
+        </div>
+    );
 };
 
 export default ActionButtons;
--- fe/src/components/Button/ButtonAddNew.jsx
+++ fe/src/components/Button/ButtonAddNew.jsx
@@ -1,17 +1,26 @@
-import { FaPlus } from "react-icons/fa";
-import { CiCirclePlus } from "react-icons/ci";
+import { FaPlus } from 'react-icons/fa';
+import { CiCirclePlus } from 'react-icons/ci';
 
 const ButtonAddNew = ({ handleAdd, label }) => {
     return (
-        <button 
+        <button
             className="btn btn-css"
-            style={{ whiteSpace: 'nowrap', background: '#0b8f50', color: '#fff'}}
+            style={{
+                whiteSpace: 'nowrap',
+                background: '#0b8f50',
+                color: '#fff',
+            }}
             onClick={handleAdd}
-        >
-            <FaPlus fontSize={14}/> <span style={{marginLeft: 5, marginBottom: 2}}>{label}</span>
+            onMouseEnter={(e) => {
+                e.currentTarget.style.background = '#0da35c';
+            }}
+            onMouseLeave={(e) => {
+                e.currentTarget.style.background = '#0b8f50';
+            }}>
+            <FaPlus fontSize={14} />{' '}
+            <span style={{ marginLeft: 5, marginBottom: 2 }}>{label}</span>
         </button>
-    )
-}
+    );
+};
 
-
-export default ButtonAddNew
+export default ButtonAddNew;
